ad7b49943d23e665a3fa26146e2cd064dafa1ac5
LlewVallis <llewvallis@gmail.com>
2020-03-14 16:07:24 +1100

    Fix bug that crashed builds

--- src/components/json-query.js
+++ src/components/json-query.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 
-import ReactJson from "react-json-view"
+import JSONTree from 'react-json-tree'
 import { primaryColor } from "../theme"
 import jmespath from "jmespath"
 
@@ -27,9 +27,30 @@ class JsonQuery extends Component {
             }
         }
 
-        if (typeof viewed !== "object" || viewed === null) {
+        if (!(viewed !== null && typeof viewed === "object") && !Array.isArray(viewed)) {
             viewed = { value: viewed }
         }
+
+        const theme = {
+            scheme: 'monokai',
+            author: 'wimer hazenberg (http://www.monokai.nl)',
+            base00: '#272822',
+            base01: '#383830',
+            base02: '#49483e',
+            base03: '#75715e',
+            base04: '#a59f85',
+            base05: '#f8f8f2',
+            base06: '#f5f4f1',
+            base07: '#f9f8f5',
+            base08: '#f92672',
+            base09: '#fd971f',
+            base0A: '#f4bf75',
+            base0B: '#a6e22e',
+            base0C: '#a1efe4',
+            base0D: '#66d9ef',
+            base0E: '#ae81ff',
+            base0F: '#cc6633'
+          }
 
         return (
             <div>
@@ -74,8 +95,11 @@ class JsonQuery extends Component {
                     padding: "3em",
                     borderRadius: "0.5em",
                     textAlign: "left",
+                        fontFamily: "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
                 }}>
-                    <ReactJson name="data" src={viewed} collapsed={true} />
+                    <JSONTree data={viewed} invertTheme={true} hideRoot={true} theme={{
+                        extend: theme,
+                    }} />
                 </div>
             </div>
         )
